6dd2f37a4e29f120ad5312e9074f6d6ffece0b70
Manvir Kaur <67894494+manvkaur@users.noreply.github.com>
2023-12-18 14:10:25 +0000

    add second project

---
+++ Tailspin.SpaceGame.Web.Second/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TailSpin.SpaceGame.Web.Second.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Tailspin.SpaceGame.Web.Second/Models/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TailSpin.SpaceGame.Web.Second.Models
+{
+    public class ProfileViewModel
+    {
+        // The player profile.
+        public Profile Profile;
+        // The player's rank according to the active filter.
+        public string Rank;
+    }
+}
